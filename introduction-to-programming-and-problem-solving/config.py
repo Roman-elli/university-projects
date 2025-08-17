74e8c2f917e055249022de8a1c6fe5d31fd6b0f4
Thales Romanelli <106693086+Roman-elli@users.noreply.github.com>
2025-08-17 14:12:29 +0100

    chore: fix info on config file

--- introduction-to-programming-and-problem-solving/config.py
+++ introduction-to-programming-and-problem-solving/config.py
@@ -1,6 +1,9 @@
+# Game config
 MAX_X = 600
 MAX_Y = 640
 DEFAULT_SIZE = 20
 SNAKE_SHAPE = 'square'
-HIGH_SCORES_FILE_PATH = 'data/high_scores.txt'
 SPEED = 0.12
+
+# File path config
+HIGH_SCORES_FILE_PATH = 'data/high_scores.txt'
