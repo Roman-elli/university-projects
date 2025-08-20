3c166332305c635ef0446c5179c151f853c185c7
Thales Lopes <thalesbrl@outlook.com>
2025-08-21 00:36:51 +0100

    chore(football-game): Finishing cleaning architecture

---
+++ introduction-to-programming/football/src/replay.py
@@ -0,0 +1,50 @@
+import main
+import os
+from config import *
+from game.game_setup import *
+
+def le_replay(nome_ficheiro):
+    coordenadas = open(f"../data/game-record/{nome_ficheiro}", 'r')
+    bola = coordenadas.readline()
+    jogador_vermelho = coordenadas.readline()
+    jogador_azul = coordenadas.readline()
+    
+    dici ={}
+    dici["bola"] = lista(bola)
+    dici["jogador_vermelho"] = lista(jogador_vermelho)
+    dici["jogador_azul"] = lista(jogador_azul)
+
+    return dici
+
+def lista(s):
+    ponto_virgula = s.split(';')
+    virgula = [item.split(',') for item in ponto_virgula]
+    lista = []
+    for i in range(0, len(virgula)-1):
+        tuplo = (float(virgula[i][0]), float(virgula[i][1]))
+        lista.append(tuplo)
+    return lista 
+
+def main():
+    print("Replays disponiveis: ")
+    if(os.path.isfile(GAME_TITLES_PATH)):
+        file = open(GAME_TITLES_PATH, 'r')
+        lista = file.readlines()
+        linhas = [linha.rstrip('\n') for linha in lista]
+        for i in range (len(linhas)):
+            print(i+1, "->", linhas[i])
+        x = int(input("Digite o replay que deseja ver: "))
+
+    estado_jogo = init_state()
+    setup(estado_jogo, False)
+   
+    replay = le_replay(linhas[x-1])
+    for i in range(len(replay['bola'])):
+        estado_jogo['janela'].update()
+        estado_jogo['jogador_vermelho']['jogador'].setpos(replay['jogador_vermelho'][i])
+        estado_jogo['jogador_azul']['jogador'].setpos(replay['jogador_azul'][i])
+        estado_jogo['bola']['bola'].setpos(replay['bola'][i])
+    estado_jogo['janela'].exitonclick()
+
+if __name__ == '__main__':
+    main()
