df25923635bbdc877daef5fbe70201553bc8d6c6
Thales Lopes <thalesbrl@outlook.com>
2025-09-14 17:51:55 +0100

    update(music-data-analysis): update config file

--- multimedia/music-data-analysis/src/config.py
+++ multimedia/music-data-analysis/src/config.py
@@ -4,10 +4,12 @@
 
 sr = 22050
 mono = True
-song_list_size = 900
+song_list_size = 200
 jump_counter = 512
 
 # =============================
 # File paths
 # =============================
 
+SAMPLES_PATH = "assets/samples"
+VALIDATION_PATH = "assets/validation"
