3c96e23e3d15451e3101cf6ffdb6a61e952a1075
Thales Lopes <thalesbrl@outlook.com>
2025-09-17 12:17:16 +0100

    update(neuroevolution): update config file

--- artificial-intelligence/lunar-lander-neuroevolution/src/config.py
+++ artificial-intelligence/lunar-lander-neuroevolution/src/config.py
@@ -1,4 +1,46 @@
 
 # =============================
-# NeuroEvolution setup
+# NeuroEvolution start setup
 # =============================
+# CONFIG
+ENABLE_WIND = False
+WIND_POWER = 15.0
+TURBULENCE_POWER = 0.0
+GRAVITY = -10.0
+RENDER_MODE = None # RENDER_MODE = 'human' for a graphic demonstration of each test
+EPISODES = 1000
+STEPS = 500
+
+nInputs = 8
+nOutputs = 2
+SHAPE = (nInputs,12,nOutputs)
+GENOTYPE_SIZE = 0
+
+POPULATION_SIZE = 100
+NUMBER_OF_GENERATIONS = 100
+
+PROB_CROSSOVER = 0.9
+
+PROB_MUTATION = 0.05
+STD_DEV = 0.1
+
+ELITE_SIZE = 1
+
+# =============================
+# Thresholds
+# =============================
+THRESHOLD_THETA = 20
+THRESHOLD_THETA_EXTREME = 90
+THRESHOLD_VY = -0.07
+THRESHOLD_Y = 0.5
+THRESHOLD_X = 0.5
+THRESHOLD_X_EXTREME = 0.005
+
+# =============================
+# Weights
+# =============================
+distance_weight = 0.4
+velocity_weight = 0.6
+theta_weight = 0.4
+contact_left_rigth = 0.7
+reward_sucess = 0
